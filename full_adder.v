e3914fd096daa5891fa1eeee20408d6a8f6c9b39
suraj4840 <144480393+suraj4840@users.noreply.github.com>
2023-09-24 13:24:22 +0530

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08/07/2023 09:21:53 AM
+// Create Date: 10/07/2023 11:08:53 AM
 // Design Name: 
-// Module Name: jk_ff_tb
+// Module Name: fa
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
